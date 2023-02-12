ec1074d1c9ad81defb6b7df0afc3741f17155750
Rokas Kasperavicius <goodname258@gmail.com>
2023-02-12 19:13:42 +0100

    removed database integration

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
 import express from 'express'
 import 'dotenv/config'
 
-import { Pool } from 'pg'
+// import { Pool } from 'pg'
 
-const pool = new Pool()
+// const pool = new Pool()
 
 const app = express()
 const port = process.env.PORT || 8080
 
 app.get('/', async (req, res) => {
-  const client = await pool.connect()
-
-  try {
-    const data = await client
-      .query('SELECT * FROM test')
-      .then((results) => results.rows)
-
-    res.send('Express + TypeScript Server' + ' id = ' + data[0].id)
-  } catch (error) {
-    console.error(error)
-  }
+  // const client = await pool.connect()
+  // try {
+  //   const data = await client
+  //     .query('SELECT * FROM test')
+  //     .then((results) => results.rows)
+  //   res.send('Express + TypeScript Server' + ' id = ' + data[0].id)
+  // } catch (error) {
+  //   console.error(error)
+  // }
   // res.send('Express + TypeScript Server')
 })
 
