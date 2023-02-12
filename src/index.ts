6ad40bc9312a5154b22fd6ed07ab2b8acad0b068
Rokas Kasperavicius <goodname258@gmail.com>
2023-02-12 16:04:07 +0100

    PostgreSQL integration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import express, { Express, Request, Response } from 'express'
 import 'dotenv/config'
 
+import { Pool } from 'pg'
+
+const pool = new Pool()
+
 const app: Express = express()
 const port = process.env.PORT || 8080
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server')
+app.get('/', async (req: Request, res: Response) => {
+  const client = await pool.connect()
+
+  try {
+    const data = await client
+      .query('SELECT * FROM test')
+      .then((results) => results.rows)
+
+    res.send('Express + TypeScript Server' + ' id = ' + data[0].id)
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 app.listen(port, () => {
