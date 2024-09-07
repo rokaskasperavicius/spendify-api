31a732cc2671a87cfb69d90be22fb4e5994b3031
Rokas Kasperavicius <rokaskasperavicius.github@gmail.com>
2024-09-07 19:56:03 +0200

    final touches

--- src/middlewares/verify-user.ts
+++ src/middlewares/verify-user.ts
@@ -7,6 +7,11 @@ import prisma from '@/services/prisma'
 export const verifyUser = async (req: ServerRequest, res: ServerResponse, next: NextFunction) => {
   const sessionToken = req.signedCookies.session as string | undefined
 
+  // https://github.com/prisma/prisma/issues/5149 xd
+  if (!sessionToken) {
+    throw new ServerError(401, ERROR_CODES.UNAUTHORIZED)
+  }
+
   const session = await prisma.sessions.findFirst({
     where: {
       session_token: sessionToken,
