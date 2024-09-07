efd1c542b1d37886849241aee663fd7dbe4621a5
Rokas Kasperavicius <rokaskasperavicius.github@gmail.com>
2024-09-08 01:11:01 +0200

    debug

--- src/routes/auth/handlers/log-out.ts
+++ src/routes/auth/handlers/log-out.ts
@@ -5,6 +5,8 @@ import prisma from '@/services/prisma'
 export const logOut = async (req: ServerRequest, res: ServerResponse) => {
   const sessionToken = req.signedCookies.session
 
+  console.log('sessionToken', sessionToken)
+
   await prisma.sessions.deleteMany({
     where: {
       session_token: sessionToken,
