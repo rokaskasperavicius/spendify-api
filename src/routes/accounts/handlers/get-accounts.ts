919a696cf0a8f69a069e65b15b1a4de221e8e898
Rokas Kasperavicius <rokaskasperavicius.github@gmail.com>
2025-05-02 23:24:22 +0200

    Added additional account details

--- src/routes/accounts/handlers/get-accounts.ts
+++ src/routes/accounts/handlers/get-accounts.ts
@@ -1,5 +1,6 @@
 import { ServerRequest, ServerResponse } from '@/lib/types'
 
+import { getAccountMetadata } from '@/services/gocardless/api'
 import { gocardlessCurrency } from '@/services/gocardless/utils/currency'
 import prisma from '@/services/prisma'
 
@@ -14,18 +15,29 @@ export const getAccounts = async (req: ServerRequest, res: ServerResponse) => {
     },
   })
 
+  // Attach extra account details
+  const accountsWithStatus = accounts.map(async (account) => {
+    const { data: metadata } = await getAccountMetadata(account.id)
+
+    return {
+      id: account.id,
+      name: account.name,
+      iban: account.iban,
+      status: metadata.status,
+      balance: gocardlessCurrency(account.balance).format(),
+      institutionId: metadata.institution_id,
+      institutionName: account.institution_name,
+      institutionLogo: account.institution_logo,
+      lastSyncedAt: account.last_synced,
+    }
+  })
+
+  const resolved = await Promise.all(accountsWithStatus)
+
   res.json({
     success: true,
     data: {
-      accounts: accounts.map((account) => ({
-        id: account.id,
-        name: account.name,
-        iban: account.iban,
-        balance: gocardlessCurrency(account.balance).format(),
-        institutionName: account.institution_name,
-        institutionLogo: account.institution_logo,
-        lastSyncedAt: account.last_synced,
-      })),
+      accounts: resolved,
     },
   })
 }
